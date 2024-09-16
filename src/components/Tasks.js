d75469dcc3692a647c0563d51e31421e792c93b7
Akash <geetadevi9956057@gmail.com>
2024-09-16 13:47:20 +0530

    it is first component

---
+++ src/components/Tasks.js
@@ -0,0 +1,47 @@
+export let Tasks = [
+    {
+      title:"Login Page",
+      description:"a full stack student login web page Lorem ipsum dolor sit, amet consectetur adipisicing elit. At labore tenetur fugiat pariatur. Reprehenderit voluptas maiores ratione, quia suscipit soluta laboriosam officia aspernatur, eveniet corrupti deleni",
+      startDate:"2024-02-31",
+      endDate:"2024-03-01",
+      taskStatus:"pending",
+      priority:"P1",
+      assigneeName:"Tom Morphy"
+    },
+    {
+      title:"Login Page",
+      description:"a full stack student login web page Lorem ipsum dolor sit, amet consectetur adipisicing elit. At labore tenetur fugiat pariatur. Reprehenderit voluptas maiores ratione, quia suscipit soluta laboriosam officia aspernatur, eveniet corrupti deleni",
+      startDate:"2024-02-31",
+      endDate:"2024-03-01",
+      taskStatus:"in-progress",
+      priority:"P1",
+      assigneeName:"Tom Morphy"
+    },
+    {
+      title:"Login Page",
+      description:"a full stack student login web page Lorem ipsum dolor sit, amet consectetur adipisicing elit. At labore tenetur fugiat pariatur. Reprehenderit voluptas maiores ratione, quia suscipit soluta laboriosam officia aspernatur, eveniet corrupti deleni",
+      startDate:"2024-02-31",
+      endDate:"2024-03-01",
+      taskStatus:"completed",
+      priority:"P1",
+      assigneeName:"Tom Morphy"
+    },
+    {
+      title:"Login Page",
+      description:"a full stack student login web page Lorem ipsum dolor sit, amet consectetur adipisicing elit. At labore tenetur fugiat pariatur. Reprehenderit voluptas maiores ratione, quia suscipit soluta laboriosam officia aspernatur, eveniet corrupti deleni",
+      startDate:"2024-02-31",
+      endDate:"2024-03-01",
+      taskStatus:"deployed",
+      priority:"P1",
+      assigneeName:"Tom Morphy"
+    }, 
+    {
+      title:"Login Page",
+      description:"a full stack student login web page Lorem ipsum dolor sit, amet consectetur adipisicing elit. At labore tenetur fugiat pariatur. Reprehenderit voluptas maiores ratione, quia suscipit soluta laboriosam officia aspernatur, eveniet corrupti deleni",
+      startDate:"2024-02-31",
+      endDate:"2024-03-01",
+      taskStatus:"deferred",
+      priority:"P1",
+      assigneeName:"Tom Morphy"
+    },
+  ]
